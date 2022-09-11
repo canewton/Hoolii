9b49508a74977879a785e6655bc4c67e6f35d2e1
canewton <cnewt2003@gmail.com>
2022-09-10 22:56:03 -0700

    scale schedule preview

--- MeetUp MessagesExtension/Components/ProfileAvailabilityPreview.swift
+++ MeetUp MessagesExtension/Components/ProfileAvailabilityPreview.swift
@@ -21,52 +21,75 @@ class ProfileAvailabilityPreview: UIView {
         if userHasEmptySchedule {
             for _ in 0..<7 {
                 let containerView = UIView()
-                let view = UIView()
-                view.layer.borderWidth = 1
-                view.layer.borderColor = AppColors.grey.cgColor
-                view.layer.cornerRadius = 3
-                containerView.addSubview(view)
-                availabilityHorizontalList.addArrangedSubview(view)
+                containerView.backgroundColor = AppColors.lightGrey
+                containerView.layer.cornerRadius = 3
+                availabilityHorizontalList.addArrangedSubview(containerView)
             }
         } else {
-            let timeBlockHeight: CGFloat = previewContainerHeight/(CGFloat(24))
+            let overallTimeRange: TimeRange = getTimeRange()
+            print(overallTimeRange)
+            let timeBlockHeight: CGFloat = (previewContainerHeight - 20)/(CGFloat(overallTimeRange.to - overallTimeRange.from))
+            print(timeBlockHeight * CGFloat(overallTimeRange.to - overallTimeRange.from))
             for i in 0..<7 {
                 let containerView = UIView()
-                let view = UIView()
+                containerView.backgroundColor = AppColors.lightGrey
+                containerView.layer.cornerRadius = 3
                 
                 for j in 0..<schedule.datesFree[i].timesFree.count {
                     let timeRange: TimeRange = schedule.datesFree[i].timesFree[j]
                     let availabilityView = UIView()
-                    view.addSubview(availabilityView)
+                    containerView.addSubview(availabilityView)
                     availabilityView.backgroundColor = AppColors.availability
                     availabilityView.translatesAutoresizingMaskIntoConstraints = false
-                    availabilityView.topAnchor.constraint(equalTo: view.topAnchor, constant: CGFloat(timeRange.from) * timeBlockHeight).isActive = true
-                    availabilityView.heightAnchor.constraint(equalToConstant: CGFloat((timeRange.to - timeRange.from)) * timeBlockHeight).isActive = true
-                    availabilityView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-                    availabilityView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+                    availabilityView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: CGFloat(timeRange.from - overallTimeRange.from) * timeBlockHeight).isActive = true
+                    availabilityView.heightAnchor.constraint(equalToConstant: CGFloat(timeRange.to - timeRange.from) * timeBlockHeight).isActive = true
+                    availabilityView.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
+                    availabilityView.rightAnchor.constraint(equalTo: containerView.rightAnchor).isActive = true
                     availabilityView.layer.cornerRadius = 3
+                    print(i)
+                    print(CGFloat(timeRange.from - overallTimeRange.from) * timeBlockHeight)
+                    print(CGFloat(timeRange.to - timeRange.from) * timeBlockHeight)
+                    print(CGFloat(timeRange.to - overallTimeRange.from) * timeBlockHeight)
+                    print((CGFloat(timeRange.to - overallTimeRange.from) * timeBlockHeight) - CGFloat(previewContainerHeight))
                     
-                    let fromLabel: UILabel = UILabel()
-                    let toLabel: UILabel = UILabel()
-                    fromLabel.text = getTime(timeRange.from)
-                    fromLabel.font = .systemFont(ofSize: 8)
-                    toLabel.text = getTime(timeRange.to)
-                    toLabel.font = .systemFont(ofSize: 8)
-                    availabilityView.addSubview(fromLabel)
-                    availabilityView.addSubview(toLabel)
-                    fromLabel.translatesAutoresizingMaskIntoConstraints = false
-                    toLabel.translatesAutoresizingMaskIntoConstraints = false
-                    fromLabel.leftAnchor.constraint(equalTo: availabilityView.leftAnchor, constant: 4).isActive = true
-                    fromLabel.rightAnchor.constraint(equalTo: availabilityView.rightAnchor).isActive = true
-                    fromLabel.topAnchor.constraint(equalTo: availabilityView.topAnchor, constant: 4).isActive = true
-                    toLabel.leftAnchor.constraint(equalTo: availabilityView.leftAnchor, constant: 4).isActive = true
-                    toLabel.rightAnchor.constraint(equalTo: availabilityView.rightAnchor).isActive = true
-                    toLabel.bottomAnchor.constraint(equalTo: availabilityView.bottomAnchor, constant: -4).isActive = true
+                    if timeRange.to - timeRange.from >= 2 {
+                        let fromLabel: UILabel = UILabel()
+                        let toLabel: UILabel = UILabel()
+                        fromLabel.text = getTime(timeRange.from)
+                        fromLabel.font = .systemFont(ofSize: 8)
+                        toLabel.text = getTime(timeRange.to)
+                        toLabel.font = .systemFont(ofSize: 8)
+                        availabilityView.addSubview(fromLabel)
+                        availabilityView.addSubview(toLabel)
+                        fromLabel.translatesAutoresizingMaskIntoConstraints = false
+                        toLabel.translatesAutoresizingMaskIntoConstraints = false
+                        fromLabel.leftAnchor.constraint(equalTo: availabilityView.leftAnchor, constant: 2).isActive = true
+                        fromLabel.rightAnchor.constraint(equalTo: availabilityView.rightAnchor).isActive = true
+                        fromLabel.topAnchor.constraint(equalTo: availabilityView.topAnchor, constant: 2).isActive = true
+                        toLabel.leftAnchor.constraint(equalTo: availabilityView.leftAnchor, constant: 2).isActive = true
+                        toLabel.rightAnchor.constraint(equalTo: availabilityView.rightAnchor).isActive = true
+                        toLabel.bottomAnchor.constraint(equalTo: availabilityView.bottomAnchor, constant: -2).isActive = true
+                    }
                 }
                 
-                containerView.addSubview(view)
-                availabilityHorizontalList.addArrangedSubview(view)
+                availabilityHorizontalList.addArrangedSubview(containerView)
             }
+        }
+        
+        func getTimeRange() -> TimeRange {
+            var minTime: Int = 24
+            var maxTime: Int = 0
+            for i in 0..<schedule.datesFree.count {
+                for j in 0..<schedule.datesFree[i].timesFree.count {
+                    if schedule.datesFree[i].timesFree[j].from < minTime {
+                        minTime = schedule.datesFree[i].timesFree[j].from
+                    }
+                    if schedule.datesFree[i].timesFree[j].to > maxTime {
+                        maxTime = schedule.datesFree[i].timesFree[j].to
+                    }
+                }
+            }
+            return TimeRange(from: minTime, to: maxTime)
         }
         
         func getTime(_ timeInput: Int) -> String {
@@ -85,6 +108,7 @@ class ProfileAvailabilityPreview: UIView {
     }
     
     class func instanceFromNib(schedule: Schedule, userHasEmptySchedule: Bool, previewContainerHeight: CGFloat) -> ProfileAvailabilityPreview? {
+        print(previewContainerHeight)
         let profileAvailabilityPreview: ProfileAvailabilityPreview? = UINib(nibName: "ProfileAvailabilityPreview", bundle: nil).instantiate(withOwner: self, options: nil)[0] as? ProfileAvailabilityPreview
         profileAvailabilityPreview?.schedule = schedule
         profileAvailabilityPreview?.userHasEmptySchedule = userHasEmptySchedule
