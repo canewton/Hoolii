cac036b72c2c11cffff9913c6c0c00f1ce732953
Caden Newton <cnewton@ucdavis.edu>
2024-08-15 08:30:04 -0400

    Working basic test

--- Hoolii MessagesExtension/Controllers/CreateProfileViewController.swift
+++ Hoolii MessagesExtension/Controllers/CreateProfileViewController.swift
@@ -10,158 +10,19 @@ import Foundation
 import UIKit
 
 class CreateProfileViewController: AppViewController, ViewControllerWithIdentifier {
-    @IBOutlet weak var backButton: BackButton!
-    @IBOutlet weak var profileIcon: UIView!
-    @IBOutlet weak var profileAvailabilityPreviewContainer: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var screenContent: UIView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var editProfileButton: UIView!
-    @IBOutlet weak var editAvailabilityButton: UIView!
-    var profileAvailabilityPreview: ProfileAvailabilityPreview!
-    var newMeetingViewController: NewMeetingViewController!
-    var userHasEmptySchedule: Bool = true
-    var prevController: UIViewController!
-    var dismissCallback: (() -> Void)!
-    
-    let defaults = UserDefaults.standard
     
     static let storyboardIdentifier = "CreateProfileViewController"
     var delegate: AnyObject?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // set initial values of the profile if a profile has not yet been created
-        StoredValues.setIfEmpty(key: StoredValuesConstants.firstName, value: "")
-        StoredValues.setIfEmpty(key: StoredValuesConstants.lastName, value: "")
-        StoredValues.setIfEmpty(key: StoredValuesConstants.userID, value: makeID(length: 20))
-        
-        backButton.configure(viewController: self)
-        
-        configureAvailabilityPreview()
-        configureProfileIcon()
-        configureNameLabel()
-        configureEditButtons()
-        generateUserAvatar()
-        
-        scrollView.contentSize = CGSize(width: view.frame.width, height: screenContent.bounds.height)
-        screenContent.frame.size.width = view.frame.width
-    }
-
-    func configureNameLabel() {
-        let firstName: String = StoredValues.get(key: StoredValuesConstants.firstName) ?? ""
-        let lastName: String = StoredValues.get(key: StoredValuesConstants.lastName) ?? ""
-        nameLabel.text = "\(firstName) \(lastName)"
     }
     
-    func configureEditButtons() {
-        let editAvailTap = UITapGestureRecognizer(target: self, action: #selector(onEditWeeklyAvailability(gesture: )))
-        editAvailabilityButton.addGestureRecognizer(editAvailTap)
-        
-        let editProfileTap = UITapGestureRecognizer(target: self, action: #selector(onEditProfile(gesture: )))
-        editProfileButton.addGestureRecognizer(editProfileTap)
-    }
-    
-    // populate the edit weekly availability screen with the user's weekly availability data
-    // navigate to the weekly availability screen
-    @objc func onEditWeeklyAvailability(gesture: UITapGestureRecognizer) {
-        let weeklyAvailabilityVC = self.storyboard?
-            .instantiateViewController(withIdentifier: "WeeklyAvailabilityInputViewController") as! WeeklyAvailabilityInputViewController
-        weeklyAvailabilityVC.userSchedule = getUserAvailability()
-        weeklyAvailabilityVC.onSaveCallback = updateAvailabilityPreview
-        self.transitionToScreen(viewController: weeklyAvailabilityVC)
-    }
-    
-    @objc func onEditProfile(gesture: UITapGestureRecognizer) {
+    @IBAction func onEditAvatar(_ sender: Any) {
         let avatarCreatorVC = self.storyboard?
             .instantiateViewController(withIdentifier: "AvatarCreatorViewController") as! AvatarCreatorViewController
-        avatarCreatorVC.editNameCallback = configureNameLabel
-        avatarCreatorVC.editProfileCallback = generateUserAvatar
         self.transitionToScreen(viewController: avatarCreatorVC)
-    }
-    
-    func generateUserAvatar() {
-        for _ in 0..<profileIcon.subviews.count {
-            profileIcon.subviews[0].removeFromSuperview()
-        }
-        
-        let storedAvatar = StoredValues.get(key: StoredValuesConstants.userAvatar)
-        if storedAvatar != nil {
-            let avatar = Avatar(jsonValue: storedAvatar!)
-            let avatarImages = AvatarImageCollection(avatar: avatar)
-            let profileIconContent = FacialFeatureOption.instanceFromNib(images: avatarImages)
-            profileIconContent.setHairColor(color: AppColors.hairColorArray[avatar.hairColor])
-            profileIconContent.setSkinColor(color: AppColors.skintoneArray[avatar.skinTone])
-            let shiftConst = avatar.getShiftConst() * profileIcon.bounds.height
-            profileIcon.addSubview(profileIconContent)
-            profileIconContent.translatesAutoresizingMaskIntoConstraints = false
-            profileIconContent.topAnchor.constraint(equalTo: profileIcon.topAnchor, constant: 8 - shiftConst).isActive = true
-            profileIconContent.leftAnchor.constraint(equalTo: profileIcon.leftAnchor, constant: 8).isActive = true
-            profileIconContent.rightAnchor.constraint(equalTo: profileIcon.rightAnchor, constant: -8).isActive = true
-            profileIconContent.bottomAnchor.constraint(equalTo: profileIcon.bottomAnchor, constant: -8 - shiftConst).isActive = true
-            
-            profileIcon.backgroundColor = AppColors.backgroundColorArray[avatar.backgroundIndex]
-        }
-    }
-    
-    // make a random id with the specified length
-    func makeID(length: Int) -> String {
-        var result = ""
-        let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-        for _ in 1...length {
-            let character = characters[characters.index(
-                characters.startIndex, offsetBy: Int.random(in: 0...(characters.count - 1))
-            )]
-            result = result + String(character)
-        }
-        return result
-    }
-    
-    // get the user stored in local storage
-    func getUser() -> User {
-        let id: String = StoredValues.get(key: StoredValuesConstants.userID)!
-        let firstName: String = StoredValues.get(key: StoredValuesConstants.firstName)!
-        let lastName: String = StoredValues.get(key: StoredValuesConstants.lastName)!
-        return User(id: id, firstName: firstName, lastName: lastName, avatar: Avatar().encodeAvatar())
-    }
-    
-    // if there is the weekly availability has not been set, return an empty scedule
-    // otherwise, decode the json string that has been stored locally and return its contents
-    func getUserAvailability() -> Schedule {
-        let jsonString: String? = StoredValues.get(key: StoredValuesConstants.userSchedule)
-        var userSchedule: Schedule = Schedule(datesFree: [
-            Day(date: ScheduleDate(0), timesFree: []),
-            Day(date: ScheduleDate(1), timesFree: []),
-            Day(date: ScheduleDate(2), timesFree: []),
-            Day(date: ScheduleDate(3), timesFree: []),
-            Day(date: ScheduleDate(4), timesFree: []),
-            Day(date: ScheduleDate(5), timesFree: []),
-            Day(date: ScheduleDate(6), timesFree: []),
-        ], user: getUser())
-        if jsonString != nil {
-            userSchedule = Schedule(jsonValue: jsonString!)
-            userHasEmptySchedule = false
-        }
-        return userSchedule
-    }
-    
-    func updateAvailabilityPreview() {
-        profileAvailabilityPreview.removeFromSuperview()
-        configureAvailabilityPreview()
-    }
-    
-    func configureAvailabilityPreview() {
-        profileAvailabilityPreview = ProfileAvailabilityPreview.instanceFromNib(schedules: [getUserAvailability()], emptySchedule: userHasEmptySchedule)!
-        profileAvailabilityPreviewContainer.addSubview(profileAvailabilityPreview)
-        profileAvailabilityPreview.translatesAutoresizingMaskIntoConstraints = false
-        profileAvailabilityPreview.leftAnchor.constraint(equalTo: profileAvailabilityPreviewContainer.leftAnchor).isActive = true
-        profileAvailabilityPreview.rightAnchor.constraint(equalTo: profileAvailabilityPreviewContainer.rightAnchor).isActive = true
-        profileAvailabilityPreview.bottomAnchor.constraint(equalTo: profileAvailabilityPreviewContainer.bottomAnchor).isActive = true
-        profileAvailabilityPreview.topAnchor.constraint(equalTo: profileAvailabilityPreviewContainer.topAnchor).isActive = true
-    }
-    
-    func configureProfileIcon() {
-        profileIcon.layer.cornerRadius = 80
     }
 }
--- Hoolii MessagesExtension/Controllers/MessagesViewController.swift
+++ Hoolii MessagesExtension/Controllers/MessagesViewController.swift
@@ -12,98 +12,6 @@ class MessagesViewController: MSMessagesAppViewController {
     
     static var currViewController: UIViewController!
     
-    override func viewDidLoad() {
-//        StoredValues.deleteKey(key: StoredValuesConstants.newMeetingOnboarding)
-//        StoredValues.deleteKey(key: StoredValuesConstants.yourAvailabilityOnboarding)
-//        StoredValues.deleteKey(key: StoredValuesConstants.hasBeenOnboarded)
-//        StoredValues.deleteKey(key: StoredValuesConstants.firstName)
-//        StoredValues.deleteKey(key: StoredValuesConstants.lastName)
-//        StoredValues.deleteKey(key: StoredValuesConstants.userAvatar)
-//        StoredValues.deleteKey(key: StoredValuesConstants.userSchedule)
-//        StoredValues.deleteKey(key: StoredValuesConstants.userID)
-//        let message = HooliiMessage(message: activeConversation?.selectedMessage)
-//        if message?.getCollectiveSchedule() != nil {
-//            AF.request("https://hoolii.fly.dev/collective-schedule?id=\(HooliiMessage.websiteScheduleID)", method: .get).validate().responseJSON(completionHandler: handleResponse)
-//        }
-    }
-    
-//    func handleResponse(_ response: AFDataResponse<Any>) {
-//        switch (response.result) {
-//            case .success(let successRes):
-//
-//            print("success")
-//            print(successRes)
-//
-//            do {
-//                let collectiveSchedule = try JSONDecoder().decode(CollectiveSchedule.self, from: response.data!)
-//                print(collectiveSchedule)
-//                debugPrint(response.data!)
-//
-//            } catch let error as NSError {
-//                print("Failed to load: \(error.localizedDescription)")
-//            }
-//
-//            case .failure(let error):
-//                print("Request error: \(error.localizedDescription)")
-//        }
-//    }
-    
-    // MARK: - Conversation Handling
-    fileprivate func composeMessage(_ collectiveSchedule: CollectiveSchedule, _ caption: String, _ session: MSSession? = nil) -> MSMessage {
-        
-        // URLComponents are a structure that parses URLs into and constructs URLs from their constituent parts
-        var components = URLComponents()
-        components.queryItems = HooliiMessage(collectiveSchedule: CollectiveSchedule.shared).queryItems
-        print(components.queryItems)
-        
-        let layout = MSMessageTemplateLayout()
-        layout.caption = caption
-        layout.image = (MessageGraphic.instanceFromNib()!).convertToImage()
-        
-        let message = MSMessage(session: session ?? MSSession())
-        message.url = components.url!
-        message.layout = layout
-        
-        return message
-    }
-    
-    public func SendMessage() {
-//        print(CollectiveSchedule.shared.toString())
-        guard let conversation = activeConversation else { fatalError("Expected a conversation") }
-        let message = composeMessage(CollectiveSchedule.shared, CollectiveSchedule.shared.meetingName, conversation.selectedMessage?.session)
-        
-        conversation.insert(message) { error in
-            if let error = error {
-                print(error)
-            }
-        }
-        
-//        print("sending post request")
-//        print(HooliiMessage(message: activeConversation?.selectedMessage)?.getCollectiveSchedule().allSchedules[0].toString())
-//        if HooliiMessage(message: activeConversation?.selectedMessage)?.getCollectiveSchedule() == nil {
-//            print(CollectiveSchedule.shared)
-//            AF.request("https://hoolii.fly.dev/collective-schedule", method: .post, parameters: CollectiveSchedule.shared).validate().responseJSON{ response in
-//                switch response.result {
-//                case .success(let data):
-//                    let jsonData = data as! [String: Any]
-//                    let id = jsonData["_id"]
-//                    conversation.insertText("http://192.168.50.161:5173/schedule/\(id!) ")
-//
-//                    AF.request("https://hoolii.fly.dev/collective-schedule?id=\(id)", method: .get).validate().responseJSON { response in
-//                        switch response.result {
-//                        case .success(let data):
-//                            print(data)
-//                        case .failure(let error):
-//                            print(error)
-//                        }
-//                    }
-//                case .failure(let error):
-//                    print(error)
-//                }
-//            }
-//        }
-    }
-    
     // MARK: Determine active view controller before extension becomes active
     override func willBecomeActive(with conversation: MSConversation) {
         super.willBecomeActive(with: conversation)
@@ -112,37 +20,10 @@ class MessagesViewController: MSMessagesAppViewController {
     
     // MARK: Determine active view controller
     private func presentViewController(for conversation: MSConversation, with presenentationStyle: MSMessagesAppPresentationStyle) {
-        // Parse a `Schedule` from the conversation's `selectedMessage` or create a new `Schedule`.
-        CollectiveSchedule.shared = HooliiMessage(message: conversation.selectedMessage)?.getCollectiveSchedule() ?? CollectiveSchedule()
-        ImageStorage.addImages(users: CollectiveSchedule.shared.allSchedules.map({ return $0.user }))
                 
         let controller: AppViewController
-        if presentationStyle == .compact {
-            if CollectiveSchedule.shared.endTime == HourMinuteTime(hour: 0, minute: 0) {
-                let onboardingCollapsedController: OnboardingCollapsedViewController = instantiateController()
-                controller = onboardingCollapsedController
-            } else {
-                let yourAvailController: YourAvailabilitiesViewController = instantiateController()
-                controller = yourAvailController
-            }
-            
-            if let currVC = MessagesViewController.currViewController as? AvatarCreatorViewController {
-                currVC.collectionViewBottomConstraint?.isActive = false
-            }
-        } else {
-            if CollectiveSchedule.shared.allSchedules.count > 0 {
-                let yourAvailabilitiesController: YourAvailabilitiesViewController = instantiateController()
-                controller = yourAvailabilitiesController
-            } else {
-                let newMeetingController: NewMeetingViewController = instantiateController()
-                controller = newMeetingController
-            }
-            
-            if let currVC = MessagesViewController.currViewController as? AvatarCreatorViewController {
-                currVC.collectionViewBottomConstraint = currVC.elemCollectionView.bottomAnchor.constraint(equalTo: currVC.bottomBar.topAnchor)
-                currVC.collectionViewBottomConstraint.isActive = true
-            }
-        }
+        let yourAvailController: CreateProfileViewController = instantiateController()
+        controller = yourAvailController
         
         // when adding the view controller, make it fill up all available space
         addChild(controller)
@@ -157,48 +38,16 @@ class MessagesViewController: MSMessagesAppViewController {
             controller.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
         
-        if presentationStyle == .expanded && (StoredValues.isKeyNil(key: StoredValuesConstants.hasBeenOnboarded)) {
-            let onboardingController: OnboardingViewController = instantiateController()
-            onboardingController.prevController = controller
-            controller.transitionToScreen(viewController: onboardingController)
-        }
-        
         controller.didMove(toParent: self)
     }
     
     // MARK: Switch view controller
     // Tells the view controller that the extension is about to transition to a new presentation style
-    override func willTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
-        super.willTransition(to: presentationStyle)
-        // when a new view controller is transitioned to, remove the view controller that is currently on the screen
-        removeAllChildViewControllers()
-    }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-
-        if(size.width > self.view.frame.size.width){
-            // screen is horizontal
-            self.dismiss(animated: false, completion: nil)
-            let horizontalOrientationController: HorizontalOrientationViewController = instantiateController()
-            addChild(horizontalOrientationController)
-            view.addSubview(horizontalOrientationController.view)
-            horizontalOrientationController.view.translatesAutoresizingMaskIntoConstraints = false
-            horizontalOrientationController.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-            horizontalOrientationController.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-            horizontalOrientationController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-            horizontalOrientationController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        } else {
-            // screen is vertical
-            for child in children {
-                if let controller = child as? HorizontalOrientationViewController {
-                    controller.willMove(toParent: nil)
-                    controller.view.removeFromSuperview()
-                    controller.removeFromParent()
-                }
-            }
-        }
-    }
+//    override func willTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
+//        super.willTransition(to: presentationStyle)
+//        // when a new view controller is transitioned to, remove the view controller that is currently on the screen
+//        removeAllChildViewControllers()
+//    }
     
     // Tells the view controller that the extenson has transitioned to a new presentation style -> compact or exmpanded
     override func didTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
@@ -208,13 +57,13 @@ class MessagesViewController: MSMessagesAppViewController {
     }
     
     // MARK: Convenience
-    private func removeAllChildViewControllers() {
-        for child in children {
-            child.willMove(toParent: nil)
-            child.view.removeFromSuperview()
-            child.removeFromParent()
-        }
-    }
+//    private func removeAllChildViewControllers() {
+//        for child in children {
+//            child.willMove(toParent: nil)
+//            child.view.removeFromSuperview()
+//            child.removeFromParent()
+//        }
+//    }
     
     func instantiateController<T: ViewControllerWithIdentifier>() -> T {
         guard var controller = storyboard?.instantiateViewController(withIdentifier: T.storyboardIdentifier)
